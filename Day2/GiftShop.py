7609174b9a79b7659146f85fc61185b9023bd38a
Victor Daniel <victorddcl@gmail.com>
2025-12-10 16:02:02 -0300

    Second Part of day two of  advent of code

--- Day2/GiftShop.py
+++ Day2/GiftShop.py
@@ -24,3 +24,37 @@ with open(r'Day2\input.txt', 'r', encoding='utf-8') as arquivo:
 
 print( soma_id)
 print("===Part one ===")  
+
+def id_invalido_completo(numero):
+    string_numero = str(numero)
+    n = len(string_numero)
+    for tam in range(1, n // 2 + 1):
+      
+        if n % tam != 0:
+            continue
+
+        bloco = string_numero[:tam]
+        repeticoes = n // tam
+
+      
+        if repeticoes >= 2 and bloco * repeticoes == string_numero:
+            return True  
+
+    return False  
+
+soma_id = 0
+with open(r'Day2\input.txt', 'r', encoding='utf-8') as arquivo:
+    for linha in arquivo:
+        linha = linha.strip()            
+
+        intervalos = linha.split(',')     
+
+        for  inter in intervalos:
+            a, b = inter.split('-')   
+            
+            for n in range(int(a), int(b) + 1):
+                if id_invalido_completo(n):
+                    soma_id += n
+
+print( soma_id)
+print("===Part Two ===")  
